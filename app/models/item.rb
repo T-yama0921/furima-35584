de25a3d74176990273072f68760bae04e6f55a0b
T-yama0921 <yamata.0921@icloud.com>
2021-05-24 15:58:11 +0900

    (修正)itemモデルにプルダウンのバリデーション付与

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,5 +6,17 @@ class Item < ApplicationRecord
   belongs_to :shipping_area
   belongs_to :shipping_days
 
-  varidates :category_id, :status_id, :shipping_charges_id, :shipping_area_id, :shipping_days_id, numericality: { other_than: 1 } 
+  with_options presence: true do
+    varidates :image
+    varidates :name
+    varidates :description
+    varidates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to, 9999999}
+    with_options numericality: { other_than: 0 } do
+      varidates :category_id 
+      varidates :status_id
+      varidates :shipping_charges_id
+      varidates :shipping_area_id
+      varidates :shipping_days_id
+    end
+  end
 end
