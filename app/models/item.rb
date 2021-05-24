9dc293fd9e7c874e69146ec1ec30679a66fdeeea
T-yama0921 <yamata.0921@icloud.com>
2021-05-24 12:42:56 +0900

    itemモデルにプルダウンのバリデーション付与
    shipping_chargesのモデル名修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,9 @@ class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :category
   belongs_to :status
-  belongs_to :shipping_charge
+  belongs_to :shipping_charges
   belongs_to :shipping_area
   belongs_to :shipping_days
+
+  varidates :category_id, :status_id, :shipping_charges_id, :shipping_area_id, :shipping_days_id, numericality: { other_than: 1 } 
 end
