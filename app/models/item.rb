0f17af1fe80256aa36c433cb557e0ee46522a63b
T-yama0921 <yamata.0921@icloud.com>
2021-05-24 23:27:02 +0900

    出品機能実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
   belongs_to :shipping_charges
@@ -7,16 +8,16 @@ class Item < ApplicationRecord
   belongs_to :shipping_days
 
   with_options presence: true do
-    varidates :image
-    varidates :name
-    varidates :description
-    varidates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to, 9999999}
+    validates :image
+    validates :name
+    validates :description
     with_options numericality: { other_than: 0 } do
-      varidates :category_id 
-      varidates :status_id
-      varidates :shipping_charges_id
-      varidates :shipping_area_id
-      varidates :shipping_days_id
+      validates :category_id 
+      validates :status_id
+      validates :shipping_charges_id
+      validates :shipping_area_id
+      validates :shipping_days_id
     end
+    validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   end
 end
