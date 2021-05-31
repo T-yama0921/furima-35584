5b2b7ad8815be1c50f7e1e9196fcadf2b354ba25
T-yama0921 <yamata.0921@icloud.com>
2021-05-31 18:41:03 +0900

    購入機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,23 +2,36 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
 
   def index
-    @order = Order.new
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
   end
 
   def create
-    @order = Order.new(order_params)
-    if @order.valid?
-      @order.save
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
       return redirect_to root_path
     else
-      render 'index'
+      render :index
     end
   end
 
   private
 
   def order_params
-    params.require(:order).permit()
+    params.require(:order_address).permit(
+      :postal_code,
+      :shipping_area_id,
+      :city,
+      :house_number,
+      :building_name,
+      :phone_number,
+      :order_id,
+    ).merge(
+      user_id: current_user.id,
+      item_id: @item.id
+    )
   end
 
 end
